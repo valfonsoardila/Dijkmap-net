f0263944072b8a2bac02844b88bd82651d35f965
VictorArdila <valfonsoardila@unicesar.edu.co>
2023-10-12 01:09:53 -0500

    Componente de enrutamiento completo

--- src/components/map/MapView.js
+++ src/components/map/MapView.js
@@ -1,16 +1,35 @@
 import React, {useState} from "react";
 import "./MapView.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, useMap  } from "react-leaflet";
 import Markers from "./markers/Markers";
 import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapMarked } from "@fortawesome/free-solid-svg-icons";
+import "leaflet-routing-machine";
 
 const MapView = () => {
   const [state, setState] = useState({
     currentLocation: { lat: "4.570868", lng: "-74.297333" },
     zoom: 6,
   });
+  function Routing() {
+    const map = useMap();
+
+    L.Routing.control({
+      waypoints: [
+        L.latLng(4.8133, -75.6961), // Pereira
+        L.latLng(4.6243, -74.0636) // Bogotá
+      ],
+      router: L.Routing.osrmv1({
+        serviceUrl: "http://router.project-osrm.org/route/v1"
+      }),
+      createMarker: function() { return null; }
+    }).addTo(map);
+
+    return null;
+  }
   return (
     <div className="container">
       <div className="title">
@@ -19,11 +38,11 @@ const MapView = () => {
       </div>
       <div className="map">
         <MapContainer center={state.currentLocation} zoom={state.zoom} >
-          <TileLayer
-            // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Markers/>
+          <Markers />
+          <Routing />
         </MapContainer>
       </div>
     </div>
