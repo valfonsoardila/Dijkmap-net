9eb970a000a23b099d2a7a3acce2f445fa77e0b8
VictorArdila <valfonsoardila@unicesar.edu.co>
2023-10-17 00:05:51 -0500

    Cambio de rutas dinamicas

--- src/components/map/MapView.js
+++ src/components/map/MapView.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./MapView.css";
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import Markers from "./markers/Markers";
@@ -13,13 +13,16 @@ import { motion } from "framer-motion";
 
 const MapView = ({ onExpandedData, cityOrigin, cityDestinity, transport }) => {
   const [originCurrent, setOriginCurrent] = useState({ lat: "", lon: "" });
-  const [destinityCurrent, setDestinityCurrent] = useState({ lat: "", lon: ""});
+  const [destinityCurrent, setDestinityCurrent] = useState({
+    lat: "",
+    lon: "",
+  });
 
   const [state, setState] = useState({
     currentLocation: { lat: "4.570868", lng: "-74.297333" },
     zoom: 6,
   });
-  
+
   function ClearRouting() {
     const map = useMap();
     map.eachLayer(function (layer) {
@@ -28,27 +31,116 @@ const MapView = ({ onExpandedData, cityOrigin, cityDestinity, transport }) => {
       }
     });
   }
+  const onSaveLocation = () => {
+    console.log(cityOrigin);
+    setOriginCurrent(cityOrigin);
+    console.log(cityDestinity);
+    setDestinityCurrent(cityDestinity);
+  };
 
   function Routing({ origin, destinity }) {
-    setOriginCurrent(cityOrigin);
-    setDestinityCurrent(cityDestinity);
     const map = useMap();
-
-    L.Routing.control({
-      waypoints: [
-        L.latLng(origin["lat"], origin["lon"]), // Valledupar
-        L.latLng(destinity["lat"], destinity["lon"]), // Mitú
-      ],
-      router: L.Routing.osrmv1({
-        //Servicio de ruteo de OSRM para desplazarme en colombia
-        serviceUrl: "http://router.project-osrm.org/route/v1",
-        profile: "car",
-      }),
-      createMarker: function () {
-        return null;
-      },
-      show: true, //onExpandedData // Add this line to hide the route information
-    }).addTo(map);
+    //Guarda los datos de origen y destino
+    useEffect(() => {
+      onSaveLocation();
+    }, [origin, destinity]);
+    //Se ejecuta cuando cambia el origen
+    useEffect(() => {
+      if (origin !== originCurrent && destinity === destinityCurrent) {
+        map.eachLayer(function (layer) {
+          if (layer instanceof L.Routing.Control) {
+            map.removeControl(layer);
+          }
+        });
+        L.Routing.control({
+          waypoints: [
+            L.latLng(origin.lat, origin.lon),
+            L.latLng(destinityCurrent.lat, destinityCurrent.lon),
+          ],
+          routeWhileDragging: true,
+          autoRoute: true,
+          showAlternatives: true,
+          altLineOptions: {
+            styles: [
+              { color: "black", opacity: 0.15, weight: 9 },
+              { color: "white", opacity: 0.8, weight: 6 },
+              { color: "blue", opacity: 0.5, weight: 2 },
+            ],
+          },
+          createMarker: function () {
+            return null;
+          },
+        }).addTo(map);
+      }
+    }, [origin, destinity, map]);
+    //Se ejecuta cuando cambia el destino
+    useEffect(() => {
+      if (origin === originCurrent && destinity !== destinityCurrent) {
+        map.eachLayer(function (layer) {
+          if (layer instanceof L.Routing.Control) {
+            map.removeControl(layer);
+          }
+        });
+        L.Routing.control({
+          waypoints: [
+            L.latLng(originCurrent.lat, originCurrent.lon),
+            L.latLng(destinity.lat, destinity.lon),
+          ],
+          routeWhileDragging: true,
+          autoRoute: true,
+          showAlternatives: true,
+          altLineOptions: {
+            styles: [
+              { color: "black", opacity: 0.15, weight: 9 },
+              { color: "white", opacity: 0.8, weight: 6 },
+              { color: "blue", opacity: 0.5, weight: 2 },
+            ],
+          },
+          createMarker: function () {
+            return null;
+          },
+        }).addTo(map);
+      }
+    }, [origin, destinity, map]);
+    //Se ejecuta cuando cambian ambos
+    useEffect(() => {
+      if (origin !== originCurrent && destinity !== destinityCurrent) {
+        map.eachLayer(function (layer) {
+          if (layer instanceof L.Routing.Control) {
+            map.removeControl(layer);
+          }
+        });
+        L.Routing.control({
+          waypoints: [
+            L.latLng(origin.lat, origin.lon),
+            L.latLng(destinity.lat, destinity.lon),
+          ],
+          routeWhileDragging: true,
+          autoRoute: true,
+          showAlternatives: true,
+          altLineOptions: {
+            styles: [
+              { color: "black", opacity: 0.15, weight: 9 },
+              { color: "white", opacity: 0.8, weight: 6 },
+              { color: "blue", opacity: 0.5, weight: 2 },
+            ],
+          },
+          createMarker: function () {
+            return null;
+          },
+          
+        }).addTo(map);
+      }
+    }, [origin, destinity, map]);
+    //Se ejecuta para usar flyto
+    useEffect(() => {
+      if (origin !== originCurrent && destinity !== destinityCurrent) {
+        map.flyTo([origin.lat, origin.lon], 7);
+      }else{
+        map.flyTo([originCurrent.lat, originCurrent.lon], 6);
+      }
+    }, [origin, destinity, map]);
+    return null;
   }
 
   return (
@@ -66,28 +158,22 @@ const MapView = ({ onExpandedData, cityOrigin, cityDestinity, transport }) => {
         <MapContainer center={state.currentLocation} zoom={state.zoom}>
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
           <Markers />
-          {
-            cityOrigin !== "" &&
-            cityDestinity !== "" &&
-            cityOrigin !== originCurrent &&
-            cityDestinity !== destinityCurrent
-              ? (
-                  console.log("Escuchó el cambio de datos"),
-                  (<Routing origin={cityOrigin} destinity={cityDestinity} />)
-                ) 
-              : cityOrigin !== originCurrent && cityDestinity === destinityCurrent
-              ? (
-                  console.log("Escuchó el cambio de origen")
-                  //(<Routing origin={cityOrigin} destinity={cityDestinity} />)
-                )
-              : cityOrigin === originCurrent && cityDestinity !== destinityCurrent
-              ? (
-                  console.log("Escuchó el cambio de destino")
-                  //(<Routing origin={cityOrigin} destinity={cityDestinity} />)
-                )
-              : console.log("No escuchó el cambio de datos")
-          }
-        )
+          {cityOrigin !== "" &&
+          cityDestinity !== "" &&
+          cityOrigin !== originCurrent &&
+          cityDestinity !== destinityCurrent
+            ? (console.log("Escuchó el cambio de datos"),
+              (<Routing origin={cityOrigin} destinity={cityDestinity} />))
+            : cityOrigin !== originCurrent && cityDestinity === destinityCurrent
+            ? (console.log("Escuchó el cambio de origen"),
+              (<ClearRouting />),
+              (<Routing origin={cityOrigin} destinity={destinityCurrent} />))
+            : cityOrigin === originCurrent && cityDestinity !== destinityCurrent
+            ? (console.log("Escuchó el cambio de destino"),
+              (<ClearRouting />),
+              (<Routing origin={originCurrent} destinity={cityDestinity} />))
+            : console.log("Terminó de escuchar")}
+          )
         </MapContainer>
       </div>
     </motion.div>
